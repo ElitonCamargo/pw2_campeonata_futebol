e6d297fbbad8d34ab9e3c567b57b523287a0af37
ELiton Camargo <camargoliveira@gmail.com>
2025-07-28 11:57:47 -0300

    Código base

---
+++ clube/listar.php
@@ -0,0 +1,75 @@
+<?php
+$host = 'localhost';
+$db   = 'campeonato_futbol';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+function conectar(): PDO {
+    global $dsn, $user, $pass, $options;
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        return $pdo;
+    } catch (\PDOException $e) {
+        echo 'Erro na conexão: ' . $e->getMessage();
+        exit;
+    }
+}
+
+function listarClubes($search=""): array {
+    $pdo = conectar();
+    // Exemplo de consulta
+    $stmt = $pdo->prepare(query: 'SELECT * FROM clube WHERE nome LIKE :search ORDER BY nome');
+    $search = "%$search%";
+    $stmt->bindValue(':search', $search, PDO::PARAM_STR);
+    $stmt->execute();
+    if($stmt->rowCount() > 0) {
+        return $stmt->fetchAll();
+    } else {
+        return [];
+    }     
+}
+
+function listarClubesJson($search="") {
+    header('Content-Type: application/json');
+    $lista = listarClubes($search);
+    if($lista) {
+        $resposta = [
+            'status' => 'success',
+            'data' => $lista
+        ];
+        http_response_code(200);
+        echo json_encode($resposta);
+    } else {
+        $resposta = [
+            'status' => 'error',
+            'message' => 'Nenhum clube encontrado.'
+        ];
+        http_response_code(404);
+        echo json_encode($resposta);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    listarClubesJson(search: $_GET['search'] ?? '');
+}
+elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+}
+elseif($_SERVER['REQUEST_METHOD'] === 'PUT'){
+    
+}
+elseif($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+    
+}
+else {
+    http_response_code(405);
+    echo json_encode(['status' => 'error', 'message' => 'Método não permitido.']);
+}
