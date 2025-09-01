4a616764740701d9a17c8a787de8cc8b74c45f34
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2025-09-01 11:23:25 +0000

    Criadno arquivo de configuração do banco

---
+++ src/api/config/config.php
@@ -0,0 +1,9 @@
+<?php
+$config_db = [
+    $host => 'db', // Nome do serviço do banco de dados no docker-compose
+    $db   => 'campeonato_futebol', // Nome do banco de dados
+    $user => 'root', // Nome do usuário do banco de dados
+    $pass => 'root', // Senha do usuário do banco de dados
+    $charset => 'utf8mb4'
+];
+
